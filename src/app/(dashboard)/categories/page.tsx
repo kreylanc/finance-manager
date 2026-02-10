9a01627fe232d98833349465b9be966b407de7dd
Devjan Pun <devjanpun@gmail.com>
2026-02-10 15:46:19 +0545

    feat: category page to edit, delete, and update

--- src/app/(dashboard)/categories/page.tsx
+++ src/app/(dashboard)/categories/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts";
+import { useNewCategory } from "@/features/categories/hooks/use-new-category";
+import { useGetCategories } from "@/features/categories/api/use-get-categories";
+import { useBulkDeleteCategories } from "@/features/categories/api/use-bulk-delete-categories";
 
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -12,26 +12,26 @@ import { DataTable } from "@/components/data-table";
 import { columns } from "./columns";
 import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Accounts() {
-  const accountQuery = useGetAccounts();
-  const accounts = accountQuery.data || []; // query for any existing accounts
-  const newAccount = useNewAccount(); // hook to use the drawer sheet for creating account
-  const bulkDelete = useBulkDeleteAccounts(); // to bulk delete rows from table
+export default function Categories() {
+  const categoryQuery = useGetCategories();
+  const categories = categoryQuery.data || []; // query for any existing accounts
+  const newCategory = useNewCategory(); // hook to use the drawer sheet for creating account
+  const bulkDelete = useBulkDeleteCategories(); // to bulk delete rows from table
 
   // disabled delete btn if account query or delete is on going
-  const isDisabled = accountQuery.isLoading || bulkDelete.isPending;
+  const isDisabled = categoryQuery.isLoading || bulkDelete.isPending;
 
   return (
     <div className="max-w-screen-2xl w-full mx-auto -mt-24">
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="flex gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-          <CardTitle className="text-xl line-clamp-1">Accounts</CardTitle>
-          <Button onClick={newAccount.onOpen}>
+          <CardTitle className="text-xl line-clamp-1">Categories</CardTitle>
+          <Button onClick={newCategory.onOpen}>
             <Plus />
             Add new
           </Button>
         </CardHeader>
-        {accountQuery.isLoading ? (
+        {categoryQuery.isLoading ? (
           <CardContent className="mt-4">
             <Skeleton className="h-10 max-w-sm" />
             <Skeleton className="h-40 w-full mt-4" />
@@ -40,7 +40,7 @@ export default function Accounts() {
           <CardContent>
             <DataTable
               columns={columns}
-              data={accounts}
+              data={categories}
               filterKey="name"
               onDelete={(rows) => {
                 // map through the array of user selected rows and get only their ids
---
+++ src/features/categories/components/category-form.tsx
@@ -0,0 +1,87 @@
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { insertCategorySchema } from "/db/schema";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+
+const formSchema = insertCategorySchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const CategoryForm = ({
+  id,
+  onSubmit,
+  defaultValues,
+  disabled,
+  onDelete,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    onSubmit(values);
+  };
+
+  const handleDelete = () => {
+    onDelete?.();
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="e.g. Cash, Bank"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" disabled={disabled}>
+          {id ? "Save Changes" : "Create category"}
+        </Button>
+        {!!id && (
+          <Button
+            type="button" // to not act as a submit button for the form
+            variant="outline"
+            className="w-full"
+            disabled={disabled}
+            onClick={handleDelete}
+          >
+            <Trash />
+            Delete category
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
